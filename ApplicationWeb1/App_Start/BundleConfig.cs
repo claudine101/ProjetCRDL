f589421ae8026562e249b15b6d6ee27377a084de
claudine100 <ndayisabaclaudine01@gmail.com>
2022-01-05 19:14:12 +0200

    mon premier commit

---
+++ ApplicationWeb1/App_Start/BundleConfig.cs
@@ -0,0 +1,61 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace ApplicationWeb
+{
+    public class BundleConfig
+    {
+        // Pour plus d’informations sur le Bundling, accédez à l’adresse http://go.microsoft.com/fwlink/?LinkId=254725 (en anglais)
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+                        "~/Scripts/jquery-ui-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.unobtrusive*",
+                        "~/Scripts/jquery.validate*"));
+
+            // Utilisez la version de développement de Modernizr pour développer et apprendre. Puis, lorsque vous êtes
+            // prêt pour la production, utilisez l’outil de génération sur http://modernizr.com pour sélectionner uniquement les tests dont vous avez besoin.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
+                        "~/Content/themes/base/jquery.ui.core.css",
+                        "~/Content/themes/base/jquery.ui.resizable.css",
+                        "~/Content/themes/base/jquery.ui.selectable.css",
+                        "~/Content/themes/base/jquery.ui.accordion.css",
+                        "~/Content/themes/base/jquery.ui.autocomplete.css",
+                        "~/Content/themes/base/jquery.ui.button.css",
+                        "~/Content/themes/base/jquery.ui.dialog.css",
+                        "~/Content/themes/base/jquery.ui.slider.css",
+                        "~/Content/themes/base/jquery.ui.tabs.css",
+                        "~/Content/themes/base/jquery.ui.datepicker.css",
+                        "~/Content/themes/base/jquery.ui.progressbar.css",
+                        "~/Content/themes/base/jquery.ui.theme.css"));
+
+            bundles.Add(new StyleBundle("~/Assets/css").Include(
+                    "~/Assets/css/bootstrap.css",
+                    "~/Assets/css/fontawesome.css",
+                    "~/Assets/css/datepicker3.css",
+                    "~/Assets/css/styles.css"
+                ));
+
+            bundles.Add(new ScriptBundle("~/Assets/js").Include(
+                    "~/Assets/js/jquery-1.11.1.js",
+                    "~/Assets/js/bootstrap.js",
+                    "~/Assets/js/chart.js",
+                    "~/Assets/js/chart-data.js",
+                    "~/Assets/js/easypiechart.js",
+                    "~/Assets/js/easypiechart-data.js",
+                    "~/Assets/js/bootstrap-datepicker.js",
+                    "~/Assets/js/custom.js"
+                ));
+        }
+    }
+}
---
+++ ApplicationWeb1/Controllers/CategorieController.cs
@@ -0,0 +1,38 @@
+﻿using ApplicationWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ApplicationWeb.Controllers
+{
+    public class CategorieController : Controller
+    {
+        //
+        // GET: /Categorie/
+        private aspnetmvc_dbEntities1 db = new aspnetmvc_dbEntities1();
+        public ActionResult Index()
+        {
+            return View(db.categories.ToList());
+        }
+        public ActionResult   create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult create([Bind(Include="ID,NomCategorie")]categorie categorie)
+        {
+            if(ModelState.IsValid)
+            {
+                db.categories.Add(categorie);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+         
+            return View(categorie);
+          }
+
+    }
+}
